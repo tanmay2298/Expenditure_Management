7b37dfe6b9c1a48e05052239be787268067a806b
Tanmay2298 <Tanmaygulati@gmail.com>
2018-10-06 13:30:58 +0530

    Report module working, needs to be integrated into frontend.py

--- report.py
+++ report.py
@@ -5,23 +5,17 @@ import numpy as np
 
 
 conn = sqlite3.connect("Expenditure.db")
-dataframe = pd.read_sql_query("SELECT * FROM Expenditure", conn)
+dataframe = pd.read_sql_query("SELECT Item, sum(Cost) as Cost FROM Expenditure group by Item", conn)
 conn.close()
 
 print(dataframe)
 
-dates = dataframe['Expenditure_Date']
+# dates = dataframe['expenditure_date']
 items = dataframe['Item']
 costs = dataframe['Cost']
 
-# dates_cost = []
-# for i in dates:
-# 	temp = i
-# 	for j in dates:
-# 		if j == i:
-# 			dates_cost.append()
 explode = ()
-for i in dates:
+for i in items:
 	explode = explode + (0,)
 
 print(explode)
@@ -34,4 +28,17 @@ ax1.pie(costs, explode = explode, labels = items, autopct='%1.1f%%',
 ax1.axis('equal')
 
 plt.tight_layout()
+plt.show()
+
+n = "05"
+conn = sqlite3.connect("Expenditure.db")
+dataframe = pd.read_sql_query("SELECT expenditure_date, sum(Cost) as cost FROM Expenditure where expenditure_date like '%-10-%' group by expenditure_date", conn)
+conn.close()
+
+print(dataframe)
+
+expenditure_dates = dataframe['expenditure_date']
+costs = dataframe['cost']
+
+plt.bar(expenditure_dates, costs)
 plt.show()
